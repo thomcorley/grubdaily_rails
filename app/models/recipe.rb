9113c4b463389b85e8817d99f4b395b8dbf3ab0c
Tom Corley <thomcorley@gmail.com>
2018-02-11 17:47:14 +0000

    Add validations and tests for Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,47 @@
 class Recipe < ApplicationRecord
+  # This is the ISO 8601 standardised time format
+  TIME_FORMAT_REGEX = /P(\d{1,2}D)?(T\d{1,2}(H|M))?(\d{1,2}(H|M))?/
+
   has_many :ingredient_sets
   has_many :method_steps
-  has_many :tags, :as => :taggable
+  has_many :tags, as: :taggable
+
+  validates :title, presence: true
+  validates :title, length: { maximum: 30 }
+  validates :summary, length: { maximum: 150 }
+  validates :introduction, presence: true
+  validates :total_time, format: { :with => TIME_FORMAT_REGEX,
+    :message => "total_time must be in valid ISO 8601 format."
+  }
+  validate :presence_of_serves_or_makes
+  validate :numericality_of_serves_or_makes
+  validates :makes_unit, length: { maximum: 20 }
+  validates :makes_unit, numericality: false
+
+  private
+  # Extra model validations
+  #
+  # Must have EITHER :serves OR :makes AND :makes_unit
+  def presence_of_serves_or_makes
+    if !serves && !makes
+      errors.add(:makes, "cannot be blank if serves is also blank")
+    elsif serves && makes
+      errors.add(:makes, "cannot be present if serves is present")
+    elsif makes && !makes_unit.present?
+      errors.add(:makes_unit,  "must be present if makes is present")
+    end
+  end
+
+ # :serves and :makes must both be numbers greater than 0. :makes_unit must NOT be a number
+  def numericality_of_serves_or_makes
+    errors.add(:serves, "must be greater than 0") if serves && serves <= 0
+    errors.add(:makes, "must be geater than 0") if makes && makes <=0
+    errors.add(:makes_unit, "cannot be a number") if  makes_unit && makes_unit.is_a?(Numeric)
+
+    if serves && !serves.is_a?(Numeric)
+      errors.add(:serves, "must be a number")
+    elsif makes && !makes.is_a?(Numeric)
+      errors.add(:makes, "must be a number")
+    end
+  end
 end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  context "Recipe creation" do
+    setup do
+      @recipe = FactoryBot.create :recipe
+    end
+
+    it "should be valid" do
+      expect(@recipe).to be_valid
+    end
+
+    it "should have a vaild title" do
+      expect(@recipe.title.length).to be < 40
+      expect(@recipe.title).to be_present
+    end
+
+    it "should have a valid summary" do
+      expect(@recipe.summary).to be_present
+      expect(@recipe.summary.length).to be < 150
+    end
+
+    it "should have a time value in valid ISO 8601 format" do
+      expect(@recipe.total_time).to match Recipe::TIME_FORMAT_REGEX
+    end
+
+    it "should have a value for either :serves or :makes" do
+      @recipe.assign_attributes(serves: nil, makes: nil, makes_unit: nil)
+      expect(@recipe).to be_invalid
+    end
+
+    it "should have a value for :makes_unit if :makes is present" do
+      @recipe.update_attributes(serves: nil, makes: 1, makes_unit: nil)
+      expect(@recipe).to be_invalid
+    end
+
+    it "should have a numerical value greater than 0 for :serves" do
+      @recipe.update_attributes(serves: 1, makes: nil, makes_unit: nil)
+      expect(@recipe.serves).to be > 0
+    end
+
+    it "should have a numerical value greater than 0 for :makes" do
+      @recipe.update_attributes(serves: 0, makes: 0, makes_unit: "loaves")
+      expect(@recipe).to be_invalid
+    end
+
+    it "should have a non-numerical value for :makes_unit" do
+      expect(@recipe.makes_unit.is_a?(Numeric)).to eq false
+    end
+  end
+end
