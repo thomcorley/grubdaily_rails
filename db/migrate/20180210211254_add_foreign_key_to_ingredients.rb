016a448b5022ba316d19dd3649d6e53025a3e52b
Tom Corley <thomcorley@gmail.com>
2018-05-08 20:35:36 -0700

    Fixed migrations

--- db/migrate/20180210211254_add_foreign_key_to_ingredients.rb
+++ db/migrate/20180210211254_add_foreign_key_to_ingredients.rb
@@ -1,5 +1,5 @@
 class AddForeignKeyToIngredients < ActiveRecord::Migration[5.1]
   def change
-    add_reference :ingredients, :ingredient_entries, foreign_key: true
+    # add_reference :ingredients, :ingredient_entries, foreign_key: true
   end
 end
