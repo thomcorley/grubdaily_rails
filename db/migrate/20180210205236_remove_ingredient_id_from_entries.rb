15776f521de1c579487487f2b26dd038c49b9b9b
Tom Corley <thomcorley@gmail.com>
2018-05-08 20:32:36 -0700

    Fixed migrations

--- db/migrate/20180210205236_remove_ingredient_id_from_entries.rb
+++ db/migrate/20180210205236_remove_ingredient_id_from_entries.rb
@@ -1,7 +1,7 @@
 class RemoveIngredientIdFromEntries < ActiveRecord::Migration[5.1]
   def change
-    # change_table :ingredient_entries do |t|
-    #   t.remove :ingredient_id
-    # end
+    change_table :ingredient_entries do |t|
+      t.remove :ingredient_id
+    end
   end
 end
