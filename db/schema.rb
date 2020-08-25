174749cc714987e4ce8f263bfb5f6977791c5699
Tom Corley <tom.corley@freeagent.com>
2020-08-25 21:17:56 +0100

    Migration for `published` column on recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_191905) do
+ActiveRecord::Schema.define(version: 2020_08_25_201716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2020_07_30_191905) do
     t.string "category"
     t.string "image_url"
     t.string "tags"
+    t.boolean "published"
   end
 
   create_table "tags", force: :cascade do |t|
