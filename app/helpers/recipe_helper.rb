a60e0772fdfe8508dd925ec22e6ba9b9de99cc3d
Tom Corley <tom.corley@freeagent.com>
2020-09-04 14:03:06 +0100

    Display serves or makes correctly for the recipe show page
    Also improves styling of the tags and most meta data.

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -1,2 +1,11 @@
 module RecipeHelper
+  def serves_or_makes_text
+    if @recipe.serves
+      "Serves: #{@recipe.serves}"
+    elsif @recipe.makes
+      "Makes #{@recipe.makes} #{@recipe.makes_unit}"
+    else
+      ""
+    end
+  end
 end
