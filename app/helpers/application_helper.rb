8eef079c4394d92e645c12d6f88472ad058c77cc
Tom Corley <thomcorley@gmail.com>
2020-10-03 22:45:53 +0100

    Add The Peels IP address to the whitelist
    This allows admin access to anyone visiting the website from the Peels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def admin_session?
-    Rails.env.development? || request.remote_ip == "82.44.245.7"
+    Rails.env.development? ||
+      request.remote_ip == "82.44.245.7" ||
+      request.remote_ip == "92.28.148.71"
   end
 end
