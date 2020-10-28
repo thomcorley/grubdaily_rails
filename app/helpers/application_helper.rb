631a5d344601559d9aa04d7ffa0a1f243ce88db3
Tom Corley <thomcorley@gmail.com>
2020-10-28 21:15:52 +0000

    Add staging env to admin check

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def admin_session?
-    Rails.env.development? ||
+    Rails.env.development? || ENV["RAILS_ENVIRONMENT"] == "staging"
       request.remote_ip == "82.44.245.7" || # 128 Ferry Road
       request.remote_ip == "92.28.148.71"   # The Peels
   end
