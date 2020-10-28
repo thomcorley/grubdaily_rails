4f0300df11ddbbf48082b33e0c36439d231d603e
Tom Corley <thomcorley@gmail.com>
2020-10-28 21:35:14 +0000

    Add REDISTOGO_URL to the config for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: ENV.fetch("REDIS_URL"), size: 1, network_timeout: 5 }
-# end
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 1, network_timeout: 5 }
+  end
 
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: ENV.fetch("REDIS_URL"), size: 12, network_timeout: 5 }
-# end
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV.fetch("REDISTOGO_URL"), size: 12, network_timeout: 5 }
+  end
+end
 
 Sidekiq::Extensions.enable_delay!
