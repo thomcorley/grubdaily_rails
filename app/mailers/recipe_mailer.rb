8899859f66440d4d218c1c2cad83409d7824a53b
Tom Corley <thomcorley@gmail.com>
2020-11-09 21:38:02 +0000

    Add logging for RecipeMailer

--- app/mailers/recipe_mailer.rb
+++ app/mailers/recipe_mailer.rb
@@ -4,5 +4,6 @@ class RecipeMailer < ApplicationMailer
   def new_recipe(recipe:, email:)
     @recipe = recipe
     mail(to: email, subject: "Something tasty for you: #{@recipe.title}")
+    Rails.logger.info("Successfully sent new recipe email for #{@recipe.title}")
   end
 end
