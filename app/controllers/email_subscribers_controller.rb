63950c4c74ffbec62cd16171c78f19fb0b9b51a2
Tom Corley <thomcorley@gmail.com>
2020-12-02 20:22:42 +0000

    Fix bug in logger in RecipeMailer

--- app/controllers/email_subscribers_controller.rb
+++ app/controllers/email_subscribers_controller.rb
@@ -2,6 +2,6 @@ class EmailSubscribersController < ApplicationController
   def delete
     @email_subscriber = EmailSubscriber.find(params[:id])
     @email_subscriber.destroy
-    Rails.logger("EmailSubscriber#{@email_subscriber.id} was unsubscribed")
+    Rails.logger.info("EmailSubscriber##{@email_subscriber.id} was unsubscribed")
   end
 end
