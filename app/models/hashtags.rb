2dc20dd462886a674da8d2a9657f55ca279514a5
Tom Corley <thomcorley@gmail.com>
2021-01-19 20:41:31 +0000

    Adds more hashtags and improves the tag generator

--- app/models/hashtags.rb
+++ app/models/hashtags.rb
@@ -1,14 +1,22 @@
 class Hashtags
+  class ArgumentError < StandardError; end
+
   class << self
     def general_tags
       file_contents = File.read("lib/hashtags/general.txt")
       file_contents.split("\n")
     end
 
-    # prints a selection of 11 tags from the general list
-    def print
+    # prints a number of tags from the general list, defaulting to 11
+    def print(number)
+      raise ArgumentError unless number.is_a?(Integer)
+
+      if number.nil?
+        number = 11
+      end
+
       output = ""
-      general_tags.sample(11).each do |tag|
+      general_tags.sample(number).each do |tag|
         output << "##{tag} "
       end
       output.strip
