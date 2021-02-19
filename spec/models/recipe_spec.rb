6f72d784d34151d285bf02dd388e6fcd379e1184
Tom Corley <thomcorley@gmail.com>
2021-02-19 09:10:15 +0000

    Fix failing specs and add a new spec file for recipe helper

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  let(:recipe) { FactoryBot.build(:recipe) }
+
   context "Recipe creation" do
-    let(:recipe) { FactoryBot.build(:recipe) }
-
-    it "should have a valid image url, if there is an image" do
-      fake_response = double("Response", code: 200)
-      allow(HTTParty).to receive(:get).and_return(fake_response)
-
-      recipe.title = "Roast Garlic Soup"
-      recipe.save!
-      expect(recipe.image_url).to be_present
-    end
-
     it "should have a vaild title" do
       expect(recipe.title.length).to be < 40
       expect(recipe.title).to be_present
@@ -51,59 +42,15 @@ RSpec.describe Recipe, type: :model do
     it "should have a non-numerical value for :makes_unit" do
       expect(recipe.makes_unit.is_a?(Numeric)).to eq false
     end
+  end
 
-    describe "#json_schema" do
-      let(:recipe) { FactoryBot.create(:recipe) }
-      let(:parsed_json) { JSON.parse(recipe.json_schema) }
-
-      it "generates valid json" do
-        expect(parsed_json).to be_truthy
-      end
-
-      it "has the correct keys" do
-        expected_keys = ["@context", "@type", "name", "author", "image"]
-        actual_keys = parsed_json.keys.first(5)
-
-        expect(expected_keys).to eq actual_keys
-      end
-
-      context "ingredients and method steps" do
-        before(:each) do
-          ingredient_set = FactoryBot.create(:ingredient_set, recipe: recipe)
-          FactoryBot.create_list(:ingredient_entry, 2, ingredient_set: ingredient_set)
-          FactoryBot.create_list(:method_step, 2, recipe: recipe)
-          recipe.reload
-        end
-
-        it "contains correct ingredients" do
-          parsed_ingredients = parsed_json["recipeIngredient"]
-          expected_ingredients = [
-            "10 g carrot, finely chopped",
-            "10 g carrot, finely chopped"
-          ]
-
-          expected_ingredients.each do |ingredient|
-            expect(parsed_ingredients).to include(ingredient)
-          end
-        end
-
-        it "contains correct method steps" do
-          parsed_method_steps = parsed_json["recipeInstructions"]
-          expected_method_steps = [
-            "Peel and finely chop the onions",
-            "Peel and finely chop the onions"
-          ]
-
-          expected_method_steps.each do |method_step|
-            expect(parsed_method_steps).to include(method_step)
-          end
-        end
-      end
+  describe "#image" do
+    it "should return the asset name" do
+      expect(recipe.image).to eq("berber-omelette")
     end
   end
 
   describe "#excerpt" do
-    let(:recipe) { FactoryBot.create(:recipe) }
     let(:expected_excerpt) {
       "An old French classic, this is truly an old school soup. It is rich and dark with beef stock and, if made correctly, should be very thick. ..."
     }
