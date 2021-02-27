0752c4dd1799552ac446289722df4e60dec4f95f
Tom Corley <thomcorley@gmail.com>
2021-02-27 08:00:44 +0000

    Update application_helper.rb
    Adds starr farm to up whitelist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     Rails.env.development? ||
       Rails.env.test? ||
       request.remote_ip == FERRY_ROAD_IP ||
-      request.remote_ip == THE_PEELS_IP
+      request.remote_ip == THE_PEELS_IP ||
+      request.remote_ip == "86.166.154.226"
   end
 end
