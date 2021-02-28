e769dcd8f5805b73cd486b27f3ee0aeb16e5a9f9
Tom Corley <thomcorley@gmail.com>
2021-02-28 11:45:45 +0000

    Removes star farm from the IP whitelist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,6 @@ module ApplicationHelper
     Rails.env.development? ||
       Rails.env.test? ||
       request.remote_ip == FERRY_ROAD_IP ||
-      request.remote_ip == THE_PEELS_IP ||
-      request.remote_ip == "86.166.154.226"
+      request.remote_ip == THE_PEELS_IP
   end
 end
