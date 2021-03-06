57462af9d8a87100d67fcf0548dfd7ba3f45bed8
Tom Corley <thomcorley@gmail.com>
2021-03-06 11:45:16 +0000

    Add test env to the admin session helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def admin_session?
     Rails.env.development? ||
+      Rails.env.test? ||
       IpAddress.admin.pluck(:ip_address).include?(request.remote_ip)
   end
 end
