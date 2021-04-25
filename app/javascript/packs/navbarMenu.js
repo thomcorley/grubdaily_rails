4d2bc906b56dea291f7b58ec40c02c0e40926831
Tom Corley <thomcorley@gmail.com>
2021-04-25 17:26:04 +0100

    Add navbar with burger menu
    * Search icon added
    * Write JS for the hamburger menu
    * added media queries for the mobile view (or use responsive.scss ?)
    * Got the desktop view margins working correctly
    * Styled the dropdown menu for full height
    * Link the search icon to the recipe index page

---
+++ app/javascript/packs/navbarMenu.js
@@ -0,0 +1,32 @@
+const burgerMenu = document.getElementById("nav-burger");
+const navMenu = document.getElementById("nav-menu");
+
+const setMenuHeight = () => {
+  if (window.screen.width < 1024) {
+    const mainContainerHeight = document.getElementById("main-container").clientHeight;
+    navMenu.style.height = `${mainContainerHeight}px`;
+  }
+};
+
+// Use the burger menu to reveal the drop-down nav menu
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+});
+
+// Set the navMenu height upon page load
+document.addEventListener("turbolinks:load", () => {
+  setMenuHeight();
+});
+
+// Ensure the height of the menu is correct after the window is resized
+window.addEventListener("resize", () => {
+  if (window.screen.width < 1024) {
+    setMenuHeight();
+  } else {
+    navMenu.style = null;
+  }
+});
+
+
+setMenuHeight();
